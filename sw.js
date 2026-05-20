1c1c4ec8673c3fa0634e6dcb1f56fcb4a2befc03
Photified <justinmainphoto@gmail.com>
2026-05-20 10:46:32 -0400

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,25 +1,15 @@
-const CACHE_NAME = 'chessrl-v3'; 
+const CACHE_NAME = 'chessrl-v4'; 
 const ASSETS_TO_CACHE = [
   './', './index.html', './styles.css', './app.js', './manifest.json'
 ];
 
 self.addEventListener('install', event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS_TO_CACHE)).then(() => self.skipWaiting())
-  );
+  event.waitUntil(caches.open(CACHE_NAME).then(c => c.addAll(ASSETS_TO_CACHE)).then(() => self.skipWaiting()));
 });
-
 self.addEventListener('activate', event => {
-  event.waitUntil(
-    caches.keys().then(cacheNames => Promise.all(
-      cacheNames.map(cache => {
-        if (cache !== CACHE_NAME) return caches.delete(cache);
-      })
-    ))
-  );
+  event.waitUntil(caches.keys().then(keys => Promise.all(keys.map(k => { if (k !== CACHE_NAME) return caches.delete(k); }))));
   return self.clients.claim(); 
 });
-
 self.addEventListener('fetch', event => {
   event.respondWith(caches.match(event.request).then(res => res || fetch(event.request)));
 });
